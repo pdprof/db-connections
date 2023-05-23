81cab3ff0e79f93b0f0495a171426685f823065f
kenoi <k_oi@hotmail.com>
2023-05-23 21:55:42 +0900

    Update script to start liberty container

--- derby-docker/ds-db2-start.sh
+++ derby-docker/ds-db2-start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --name db2-connections -p 9443:9443 -p 9080:9080 -v ~/pdprof:/pdprof db2-connections
+docker run -d --name db2-connections -p 9443:9443 -p 9080:9080 db2-connections
